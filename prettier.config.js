23b941c6a454d000da772a84c65418e821763e44
gylesfohl <gyles.fohl@thomsonreuters.com>
2024-03-29 14:36:38 -0500

    config

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 120,
+  proseWrap: 'always',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
