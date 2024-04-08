187775547a069b205f1b24acda2dcf9305071036
gylesfohl <gyles.fohl@thomsonreuters.com>
2024-04-07 23:01:17 -0500

    try using globsync again

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,9 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { globSync } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import { resolve } from 'path';
 
@@ -22,16 +25,25 @@ export default defineConfig({
         'clsx',
         '@heroicons/react',
       ],
+      input: Object.fromEntries(
+        globSync('src/**/*.{ts,tsx}', { ignore: 'src/**/*.stories.tsx' }).map(file => [
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
+      ),
       output: {
-        preserveModules: true,
-        preserveModulesRoot: 'src',
         format: 'es',
-        entryFileNames: '[name].js',
         dir: 'dist',
+        entryFileNames: '[name].js',
       },
     },
   },
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), dts({ rollupTypes: true, exclude: ['src/**/*.stories.tsx'] })],
+  copyPublicDir: false,
   css: {
     postcss: {
       plugins: [tailwindcss],
